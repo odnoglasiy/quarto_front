e2ad04039f2f4271b1dc0c4608cf18c791277fe7
odnoglasiy <odnoglasiy@gmail.com>
2021-05-17 21:14:59 +0300

    One page patch

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-    var active_figure_id="figure17",previous_figure_id="figure17",player_name = "Pirate",game_id="lol";
+    var active_figure_id="figure17",previous_figure_id="figure17",player_name = "Pirate",game_id="",game_dif;
     let moves = [];
 /*This function transform i into 4-digit id*/
 function i_to_binary(r){
@@ -18,6 +18,9 @@ function human_translate(cell){
 
 /*This function set all images to figure's cells and set to all TD's them functions*/
 function set_figures(game_difficulty){
+    game_dif=game_difficulty;
+    document.getElementsByClassName("game_board")[0].style.display="block";
+    document.getElementsByClassName("choose__intro")[0].style.display="none";
     var figure_id,pic_link,figure;
     for(var i=0;i<16;i++){
         figure_id="figure"+i;
@@ -27,9 +30,10 @@ function set_figures(game_difficulty){
         figure.style.backgroundImage=pic_link;
         figure.onclick = function(){give_figure(this.id)};
     }
-
     var chess_cells = document.getElementById('chess_board').getElementsByTagName('TD');
-    for(var i=0;i<chess_cells.length;i++) chess_cells[i].onclick = function(){make_move(this.id, true)}
+    for(var i=0;i<chess_cells.length;i++) {
+        chess_cells[i].onclick = function(){make_move(this.id, true)}
+    }
     start_game(game_difficulty);
 }
 
@@ -48,9 +52,15 @@ async function start_game(game_difficulty){
       
       let commits = await response.json();
       game_id = commits.gameId;
-      if(commits.firstPlayer) console.log( commits.firstPlayer + " moves first");
+      if(commits.firstPlayer){
+            console.log( commits.firstPlayer + " moves first");
+            document.getElementById("first_turn").innerHTML=player_name;
+            document.getElementById("second_turn").innerHTML=game_difficulty;
+      }
       else {
         console.log("Computer moves first");
+        document.getElementById("first_turn").innerHTML=game_difficulty;
+        document.getElementById("second_turn").innerHTML=player_name;
           active_figure_id="figure"+commits.nextMove.piece;
           make_move("cell"+commits.nextMove.row+commits.nextMove.column, false)     
 }
@@ -131,6 +141,7 @@ function watch_history(){
     document.getElementById("moves_menu").style.display="block"
     for(i=0;i<(moves.length/2);i++){
          document.getElementById("move"+(i+1)).textContent=""+moves[i*2]+" to "+moves[i*2+1]
+         document.getElementById("move"+(i+1)).onclick=function(){show_move(this.id)}
     }
     for(i=moves.length/2;i<16;i++) {
         document.getElementById("move"+(i+1)).style.display="none"
@@ -167,4 +178,41 @@ function refresh_status(cell_id,figure_id){
     figure_id = i_to_binary(Number(figure_id.replace(/[^0-9]/g,'')));
     moves.push(figure_id,cell_id)
     document.getElementById("status").innerHTML="Computer plays "+figure_id+" to "+cell_id;
+}
+
+function show_move(move_id){
+    var chess_cells = document.getElementById('chess_board').getElementsByTagName('TD');
+    for(var i=0;i<chess_cells.length;i++){
+        chess_cells[i].style.backgroundImage="";
+        if(chess_cells[i].className=="black") chess_cells[i].style.backgroundColor="#6786b8";
+        else chess_cells[i].style.backgroundColor="#d1dced"
+    }
+}
+
+function play_again(){
+    document.getElementsByClassName("chess")[0].style.pointerEvents="auto";
+    document.getElementsByClassName("chess")[1].style.pointerEvents="auto";
+    var chess_cells = document.getElementsByTagName('TD');
+    for(var i=0;i<chess_cells.length;i++){
+        chess_cells[i].style.backgroundImage="";
+        chess_cells[i].style.pointerEvents="auto";
+        
+        if(chess_cells[i].className=="black") chess_cells[i].style.backgroundColor="#6786b8";
+        else chess_cells[i].style.backgroundColor="#d1dced"
+    }
+    for(var i=0;i<16;i++){
+        figure_id="figure"+i;
+        pic_link="url('assets/images/" + figure_id;
+        pic_link+=".png')"
+        figure=document.getElementById(figure_id);
+        figure.style.backgroundImage=pic_link;
+        figure.onclick = function(){give_figure(this.id)};
+        document.getElementById("modal_menu").style.display="none";
+        document.getElementById("moves_menu").style.display="none";
+        document.getElementsByClassName("chess")[0].style.display="block"
+        document.getElementsByClassName("chess")[2].style.display="block"
+
+    }
+    moves=[];
+    start_game(game_dif)
 }
